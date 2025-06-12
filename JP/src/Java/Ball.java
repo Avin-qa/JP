522252e34b748e64dbadc1ada48a2bf29efafa31
Avinash <Avinash@DESKTOP-EAL55EL>
2025-06-12 23:28:12 +0530

    added new file Ball

---
+++ JP/src/Java/Ball.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class Ball {
+
+	public static void main(String[] args) {
+		
+		System.out.println("All Balls are Green");
+		System.out.println(4+9);
+		System.out.println(8*7);
+	}
+
+}
