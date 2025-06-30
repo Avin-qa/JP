062c729ccbec9e1a4bdacb54dd8c2447d6f2e35b
Avinash <Avinash@DESKTOP-EAL55EL>
2025-06-30 15:45:09 +0530

    Change the color name

--- JP/src/Java/Ball.java
+++ JP/src/Java/Ball.java
@@ -4,7 +4,7 @@ public class Ball {
 
 	public static void main(String[] args) {
 		
-		System.out.println("All Balls are Green");
+		System.out.println("All Balls are Green color");
 		System.out.println(4+9);
 		System.out.println(8*7);
 	}
